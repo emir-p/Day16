303fad526d4df7ea143a2de1c7f23e4be07de6ad
emcho87 <56119418+emcho87@users.noreply.github.com>
2021-03-24 09:50:42 +0100

    Import a Default Export

---
+++ ex09/src/Person.js
@@ -0,0 +1,17 @@
+class Person {
+    constructor(name, email, age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    getPerson() {
+        var personInfo = `Name: ${this.name}, email: ${this.email}, age: ${this.age}`;
+        return personInfo;
+    }
+
+}
+
+let Emir = new Person("Emir", "em.peljto@gmail.com", 23);
+console.log(Emir.getPerson());
+module.exports = Person;
